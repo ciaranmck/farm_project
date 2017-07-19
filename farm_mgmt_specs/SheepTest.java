6cecb960a9a99ac7d8c856d562981693012f672e
Ciaran McKenna <ciarantsc@gmail.com>
2017-07-19 14:40:44 +0100

    equivalent test ran and passed for sheep class

---
+++ farm_mgmt_specs/SheepTest.java
@@ -0,0 +1,38 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+import farm_mgmt.*;
+
+public class SheepTest {
+
+  Sheep sheep;
+
+  @Before
+  public void before() {
+    sheep = new Sheep();
+  }
+
+@Test
+public void testSheepHasName() {
+  sheep.setName("Dolly");
+  assertEquals("Dolly", sheep.getName());
+}
+
+@Test
+public void testSheepHasAge() {
+  sheep.setAge(2);
+  assertEquals(2, sheep.getAge());
+}
+
+@Test 
+public void testSheepHasColour() {
+  sheep.setColour("White");
+  assertEquals("White", sheep.getColour());
+}
+
+@Test
+public void testSheepHasGender() {
+  sheep.setGender("F");
+  assertEquals("F", sheep.getGender());
+}
+
+}
