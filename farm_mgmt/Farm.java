5c0f843c2fda7af74fabbc70072401686ef987b0
Ciaran McKenna <ciarantsc@gmail.com>
2017-07-19 14:47:31 +0100

    Farm class made

---
+++ farm_mgmt/Farm.java
@@ -0,0 +1,17 @@
+package farm_mgmt;
+
+public class Farm {
+
+  private String name;
+  private Arraylist<Sheep> sheepPen;
+  private Arraylist<Alpaca> alpacaPen;
+
+  public Farm() {
+    this.name = name;
+    this.sheepPen = new Arraylist<Sheep>();
+    this.alpacaPen = new Arraylist<Alpaca>();
+  }
+
+  
+
+}
